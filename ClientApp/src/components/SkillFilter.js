42bc9a5c8a50232a8517d54a0afa7c5394fba281
masan.sato <masan.sato@lincrea.co.jp>
2023-11-14 00:11:52 +0900

    フィルターが機能するように修正

--- ClientApp/src/components/SkillFilter.js
+++ ClientApp/src/components/SkillFilter.js
@@ -8,7 +8,6 @@ const SkillFilter = ({ onFilterChange }) => {
                 <option value="all">全て</option>
                 <option value="skill1">スキル1</option>
                 <option value="skill2">スキル2</option>
-                {/* 他のスキルオプション */}
             </select>
         </div>
     );
