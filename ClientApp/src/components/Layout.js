4048aa80fef0d01b17428b9d2262aab4cad967d4
masan.sato <masan.sato@lincrea.co.jp>
2023-11-13 18:25:00 +0900

    コミットします

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
-import SkillFilter from './SkillFilter';
-import SkillScatterPlot from './SkillScatterPlot';
 
 export class Layout extends Component {
     static displayName = Layout.name;
@@ -11,8 +9,6 @@ export class Layout extends Component {
         return (
             <div>
                 <NavMenu />
-                <SkillFilter />  {/* SkillFilter コンポーネントを追加 */}
-                <SkillScatterPlot />  {/* SkillScatterPlot コンポーネントを追加 */}
                 <Container tag="main">
                     {this.props.children}
                 </Container>
--- ClientApp/src/components/SkillScatterPlot.js
+++ ClientApp/src/components/SkillScatterPlot.js
@@ -8,25 +8,26 @@ const mockData = [
     // 他のスキルデータ
 ];
 
-const SkillScatterPlot = () => {
+const SkillScatterPlot = ({ data }) => {
     const svgRef = useRef();
 
     useEffect(() => {
         const svg = d3.select(svgRef.current);
         svg.selectAll("*").remove(); // 既存の描画をクリア
 
-        // モックデータを使って散布図を描画
+        // フィルタリングされたデータを使って散布図を描画
         svg.selectAll("circle")
-            .data(mockData)
+            .data(data)
             .enter()
             .append("circle")
             .attr("cx", d => d.level * 40) // 仮のX座標
             .attr("cy", d => Math.random() * 200) // 仮のY座標
             .attr("r", 5) // 半径
             .style("fill", "blue");
-    }, []);
+    }, [data]);
 
     return <svg ref={svgRef} width="400" height="400" />;
 };
+
 
 export default SkillScatterPlot;
