e4e4c8ea7b02e8a72c58dde33759de0789f3675d
y.katae <y.katae@lincrea.co.jp>
2023-11-22 23:44:16 +0900

    スキル詳細画面用

---
+++ ClientApp/src/components/skillDetail/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class index extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Sample</p>
+            </div>
+        );
+    }
+}
+
+export default index;
